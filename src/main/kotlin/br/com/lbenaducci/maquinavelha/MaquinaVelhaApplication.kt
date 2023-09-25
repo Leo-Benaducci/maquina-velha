039f2fee321351670d51c633119df4ad6c5cc8d1
Leo Benaducci <lbenaducci.dev@gmail.com>
2023-09-25 17:46:11 -0300

    Doc: Adding swagger config

--- src/main/kotlin/br/com/lbenaducci/maquinavelha/MaquinaVelhaApplication.kt
+++ src/main/kotlin/br/com/lbenaducci/maquinavelha/MaquinaVelhaApplication.kt
@@ -1,9 +1,12 @@
 package br.com.lbenaducci.maquinavelha
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@OpenAPIDefinition(info = Info(title = "Maquina Velha", version = "0.0.1"))
 class MaquinaVelhaApplication
 
 fun main(args: Array<String>) {
