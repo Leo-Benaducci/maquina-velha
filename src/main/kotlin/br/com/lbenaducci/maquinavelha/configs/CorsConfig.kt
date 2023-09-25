fc49b97e65517f0f1bc92a9b67b2edd03547f1af
Leo Benaducci <lbenaducci.dev@gmail.com>
2023-09-25 17:59:39 -0300

    Fix: Removing CORS error

---
+++ src/main/kotlin/br/com/lbenaducci/maquinavelha/configs/CorsConfig.kt
@@ -0,0 +1,13 @@
+package br.com.lbenaducci.maquinavelha.configs
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+    }
+}
